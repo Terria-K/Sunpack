0755ffb451b1982b74ea3cc2b1808bfc601d6ff2
Terria-K <aelbrian028@gmail.com>
2024-07-17 20:18:43 +0800

    Updated install script

--- scripts/install-to-bin.sh
+++ scripts/install-to-bin.sh
@@ -1,3 +1,9 @@
-dotnet publish ../Sunpack.csproj -c Release --self-contained
+# dotnet publish ../Sunpack.csproj -c Release --self-contained
 cd "../bin/Release/net8.0/linux-x64/publish"
-sudo cp ./sunpack /usr/local/bin
+mkdir ~/.local/sunpack
+mkdir ~/.local/sunpack/bin
+mkdir ~/.local/sunpack/lib
+sudo cp ./sunpack ~/.local/sunpack/bin
+sudo cp ./liblua54.so ~/.local/sunpack/lib
+
+sudo ln -s ~/.local/sunpack/bin/sunpack /usr/local/bin/sunpack
